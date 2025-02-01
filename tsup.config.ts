225a6a4a492ffe560c539d7a62ce0f3b766f5cec
Adam Fratino <fratino.a@gmail.com>
2025-02-01 16:42:15 -0500

    rename styles, update README

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts", "src/core.css"],
-	treeshake: false,
-	sourcemap: "inline",
-	minify: true,
-	clean: true,
-	dts: true,
-	splitting: false,
-	format: ["esm"],
-	loader: { ".css": "copy" },
-	external: ["cva", "tailwind-merge"],
-	injectStyle: false,
+  entry: ["src/index.ts", "src/styles.css"],
+  treeshake: false,
+  sourcemap: "inline",
+  minify: true,
+  clean: true,
+  dts: true,
+  splitting: false,
+  format: ["esm"],
+  loader: { ".css": "copy" },
+  external: ["cva", "tailwind-merge"],
+  injectStyle: false,
 });
