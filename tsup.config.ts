37547f1e52c4aad01cebdf697be21fa4f89352d1
Adam Fratino <fratino.a@gmail.com>
2025-02-15 12:39:56 -0500

    bump

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/constants/index.ts", "src/globals.css"],
+  entry: [
+    "src/index.ts",
+    "src/constants/index.ts",
+    "src/storybook/index.ts",
+    "src/globals.css",
+  ],
   treeshake: false,
   sourcemap: "inline",
   minify: true,
