30c7f4f47bf370c9fb6368eae73b135dc9676cfb
Adam Fratino <fratino.a@gmail.com>
2025-02-15 14:44:09 -0500

    fix disabled

--- src/storybook/disableArgTypes.ts
+++ src/storybook/disableArgTypes.ts
@@ -1,5 +1,16 @@
-export const disableArgTypes = (...properties: string[]) => {
-  return properties.map((property, i) => ({
-    [property]: { table: { disable: true } },
-  }));
+export type DisabledArgTypes = {
+  [key: string]: {
+    table: {
+      disable: boolean;
+    };
+  };
+};
+
+export const disableArgTypes = <T>(
+  ...keys: Array<keyof T>
+): DisabledArgTypes => {
+  return keys.reduce((acc, key) => {
+    acc[key as string] = { table: { disable: true } };
+    return acc;
+  }, {} as DisabledArgTypes);
 };
