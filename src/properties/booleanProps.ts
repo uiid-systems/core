535390594d761a771f3ca71ab936b515b9f34886
Adam Fratino <fratino.a@gmail.com>
2025-02-17 01:23:02 -0500

    change boolean

--- src/properties/booleanProps.ts
+++ src/properties/booleanProps.ts
@@ -1,10 +1,16 @@
-export const booleanProps = <T extends readonly string[]>(
-  props: Record<string, any>,
-  booleanProperties: T
-) => {
-  const classes = booleanProperties
-    .filter((key) => props[key] === true)
-    .join(" ");
+type StyleObject = Record<string, React.CSSProperties>;
 
-  return classes;
+export const booleanProps = (
+  props: Record<string, any>,
+  styleObjects: StyleObject
+) => {
+  return Object.entries(props)
+    .filter(([key, value]) => value === true && key in styleObjects)
+    .reduce(
+      (acc, [key]) => ({
+        ...acc,
+        ...styleObjects[key],
+      }),
+      {} as React.CSSProperties
+    );
 };
