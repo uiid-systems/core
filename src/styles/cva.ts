e4432558d1adc8e2922b0b781a7c5e4e88afe389
Adam Fratino <fratino.a@gmail.com>
2025-04-22 17:28:47 -0400

    cx no empty classname

--- src/styles/cva.ts
+++ src/styles/cva.ts
@@ -1,10 +1,18 @@
 import { defineConfig, type VariantProps } from "cva";
 import { twMerge } from "tailwind-merge";
 
-export const { cva, cx, compose } = defineConfig({
+const config = defineConfig({
   hooks: {
     onComplete: (className) => twMerge(className),
   },
 });
+
+export const cva = config.cva;
+export const compose = config.compose;
+
+export function cx(...args: Parameters<typeof config.cx>): string | undefined {
+  const merged = config.cx(...args);
+  return merged.trim() ? merged : undefined;
+}
 
 export type { VariantProps };
