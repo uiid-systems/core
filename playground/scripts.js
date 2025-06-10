1cace69953ee8ead5a1363b47d4ef46de491133a
Adam Fratino <fratino.a@gmail.com>
2025-06-09 21:17:04 -0400

    fun token stuff

---
+++ playground/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("theme-toggle").addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  });
+});
