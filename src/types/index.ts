b9f278470700b85929563d8b7e723b7dfa9a42e2
Adam Fratino <fratino.a@gmail.com>
2025-08-04 11:57:59 -0400

    fix: merge conflict

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,3 @@
 export type RenderProp = React.ReactElement<
   React.PropsWithChildren<{ className?: string; style?: React.CSSProperties }>
 >;
-<<<<<<< HEAD
-
-export type Uiid = {
-  uiid?: string;
-};
-=======
->>>>>>> b3c0885 (cleanup, slim down)
